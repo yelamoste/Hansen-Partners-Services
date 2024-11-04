b4094c739bb389c2c601bf57b6c3dfad198c14eb
yelamoste <108941678+yelamoste@users.noreply.github.com>
2024-11-05 03:34:56 +0800

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,64 @@ function App() {
   const [Apmt, setApmt] = useState(false);
 
   return (
-    <> 
+    <>
 
       <div className="main-cont">
-        <Navbar/>
+        <Navbar />
         <div className="front-page">
           <div className="image-bg">
-            <img src={placeholder} className='design-bg' id='pic2'/>
-            <img src={star} className='design-bg' id='pic3'/>
-            <img src={stars} className='design-bg' id='pic4'/>
-            <img src={urban} className='design-bg' id='pic5'/>
+            <img src={placeholder} className='design-bg' id='pic2' />
+            <img src={star} className='design-bg' id='pic3' />
+            <img src={stars} className='design-bg' id='pic4' />
+            <img src={urban} className='design-bg' id='pic5' />
           </div>
           <p className="page-name">Services</p>
-          <button onClick={()=> setApmt(true)} className='apmtsty'>Book an Appointment</button>
-          <ApmtButton trigger={Apmt} setTrigger={setApmt}/>
-          
+          <button onClick={() => setApmt(true)} className='apmtsty'>Book an Appointment</button>
+          <ApmtButton trigger={Apmt} setTrigger={setApmt} />
+
         </div>
         <div className="image-bg-full"></div>
-        <div className="cmp-section"></div>
+        <div className="cmp-section">
+          <div className="title-compre">
+            <p className="text-cont-title">Comprehensive Market Plan</p>
+            <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/hqguvxyyi5jciyrzy35v/page-3" alt="" className="author" />
+          </div>
+          <div className="main-text-cont">
+            <div className="text-cont">
+              <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/rmtn3r5zpsljdsncbwa4/image-007" alt="Responsive" className='image-logo-compre' />
+              <p className="title">Responsive</p>
+              <p className="details">I am always available via phone, text, or email.</p>
+            </div>
+            <div className="text-cont">
+              <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/ks8ztlvw2b35q5qgs6xn/image-011" alt="Syndication" className='image-logo-compre' />
+              <p className="title">Syndication</p>
+              <p className="details">I market your property locally, nationally, and internationally</p>
+            </div>
+            <div className="text-cont">
+              <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/zr3bugldblvebixagfs1/image-009" alt="Virtual Tour" className='image-logo-compre' />
+              <p className="title">Virtual Tour</p>
+              <p className="details">Let's make your home stand out with a high quality virtual tour.</p>
+            </div>
+            <div className="text-cont">
+              <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/dywgmsgzddtcsqxagbwh/image-013" alt="Photography" className='image-logo-compre' />
+              <p className="title">Photography</p>
+              <p className="details">Beautiful, high-end photography is a central part of our marketing plan for your property.</p>
+            </div>
+          </div>
+          <div className="card-cont">
+            <div className="card-cont-cont">
+              <img src="https://res.cloudinary.com/luxuryp/images/w_1280,c_limit,f_auto,q_auto/ghtq7jtce7tsgdjtosrs/image-025" alt="" className='author-img' />
+              <div className="cont-title"></div>
+              <div className="cont-details"></div>
+            </div>
+
+            <div className="card-cont-cont">
+              <div className="cont-title"></div>
+              <div className="cont-details"></div>
+            </div>
+          </div>
+
+        </div>
         <div className="processes"></div>
         <div className="brief-info"></div>
         <div className="footer"></div>
