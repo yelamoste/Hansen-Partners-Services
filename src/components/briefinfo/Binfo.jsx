2f00449878bb366a164d72f8b01bd3114a5148b7
yelamoste <108941678+yelamoste@users.noreply.github.com>
2024-11-06 04:19:34 +0800

    update 4

--- src/components/briefinfo/Binfo.jsx
+++ src/components/briefinfo/Binfo.jsx
@@ -3,7 +3,11 @@ import "../briefinfo/Binfo.css"
 
 const Binfo = () => {
   return (
-    <div className="brief-info"></div>
+    <div className="brief-info">
+      <img src="https://res.cloudinary.com/luxuryp/images/f_auto,q_auto/azjfmtzx6evxpubynfci/edit-page-11r2" alt="" className="img-cont-33" />
+      <p className="title">Over 33 Years of Real Estate Success</p>
+      <p className="details">We provide every one of our clients with a level of service they won’t find anywhere else. We give them what they need, often before they know they need it. ​​​​​​​In real estate, almost everything can be negotiated. When you choose Hansen Partners It is experience is 100% nonnegotiable. ​​​​​​​ ​​​​​​​And it’s an experience like no other.</p>
+    </div>
   )
 }
 
