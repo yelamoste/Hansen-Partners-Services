5f1da90b89bbdcdd8147d7e0b4479dea727fad3a
yelamoste/ <108941678+yelamoste@users.noreply.github.com>
2025-09-04 16:22:36 +0800

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='nav-cont'>
+    <div className='nav-main'>
       <div className='hansen-title'>Hansen Partners Services</div>
-        <Link to="/" className='dir'>Home</Link>
+      <div className='nav-cont'>
+      <Link to="/" className='dir'>Home</Link>
         <Link to="/meet-the-team" className='dir'>Meet the Team</Link>
         <Link to="/search-for-homes" className='dir'>Search For Homes</Link>
         <Link to="/our-communities" className='dir'>Our Communities</Link>
@@ -15,7 +16,7 @@ const Navbar = () => {
         <Link to="/home-across-america" className='dir'>Home Across America</Link>
         <Link to="/testimonials" className='dir'>Testimonials</Link>
         <Link to="/contact-us" className='dir'>Contact Us</Link>
-
+      </div>
     </div>
   )
 }
